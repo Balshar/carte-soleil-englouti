961da37100985c8f0a524b6e6cfbd2b65fa158fa
Balshar <adrien.mangeol@free.fr>
2026-02-24 17:53:11 +0100

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,16 +1,14 @@
+// /api/notion.js
 export default async function handler(req, res) {
-  const response = await fetch(
-    `https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`,
-    {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${process.env.NOTION_TOKEN}`,
-        "Notion-Version": "2022-06-28",
-        "Content-Type": "application/json"
-      }
-    }
-  );
-
-  const data = await response.json();
-  res.status(200).json(data);
+    const response = await fetch(`https://api.notion.com/v1/databases/${process.env.DATABASE_ID}/query`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.NOTION_TOKEN}`,
+            'Notion-Version': '2022-06-28',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({}),
+    });
+    const data = await response.json();
+    res.status(200).json(data);
 }
